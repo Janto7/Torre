2ae283c1dbbb751de06d1a5b1da90afd8bfd64c8
Janto7 <72363448+Janto7@users.noreply.github.com>
2021-11-14 23:27:32 +0100

    Fix Commit anterior

--- src/main/java/org/iesalandalus/programacion/torreajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/Posicion.java
@@ -2,7 +2,9 @@ package org.iesalandalus.programacion.torreajedrez;
 
 public class Posicion {
 	//Atributos privados como nos indica el diagrama
-	Private int fila;
-	Private char columna;
+	private int fila;
+	private char columna;
 
 }
+
+
