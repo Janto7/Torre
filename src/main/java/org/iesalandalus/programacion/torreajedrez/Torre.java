96d67ef0dab9cd99341ea3d4cea15ae6941d946a
Janto7 <72363448+Janto7@users.noreply.github.com>
2021-11-15 00:16:42 +0100

    Commit clase Torre visibilidad adecuada

---
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/Torre.java
@@ -0,0 +1,10 @@
+package org.iesalandalus.programacion.torreajedrez;
+
+public class Torre {
+	
+	// La visibilidad adecuada es privada porque son atributos de clase
+	private Color color;
+	private Posicion posicion;
+	
+
+}
