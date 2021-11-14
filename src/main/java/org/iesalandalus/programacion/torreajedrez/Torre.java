d0fa3e6736ed6a80bde52dacd5c998641ae7a4f1
Janto7 <72363448+Janto7@users.noreply.github.com>
2021-11-15 00:29:27 +0100

    Commit Métodos set and get clase Torre

--- src/main/java/org/iesalandalus/programacion/torreajedrez/Torre.java
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/Torre.java
@@ -1,10 +1,28 @@
 package org.iesalandalus.programacion.torreajedrez;
 
 public class Torre {
-	
+
 	// La visibilidad adecuada es privada porque son atributos de clase
 	private Color color;
 	private Posicion posicion;
-	
+
+	public Color getColor() {
+		return color;
+	}
+
+	private void setColor(Color color) {
+		if (color == null) {
+			throw new IllegalArgumentException("ERROR: No se puede asignar un color nulo.");
+		}
+		this.color = color;
+	}
+
+	public Posicion getPosicion() {
+		return posicion;
+	}
+
+	private void setPosicion(Posicion posicion) {
+		this.posicion = posicion;
+	}
 
 }
