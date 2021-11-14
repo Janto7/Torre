a474d17ca19c2d05b4f662b7f54d7336e2174aca
Janto7 <72363448+Janto7@users.noreply.github.com>
2021-11-14 23:36:19 +0100

    Commit metodos get y set

--- src/main/java/org/iesalandalus/programacion/torreajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/Posicion.java
@@ -1,10 +1,37 @@
 package org.iesalandalus.programacion.torreajedrez;
 
 public class Posicion {
-	//Atributos privados como nos indica el diagrama
+	// Atributos privados como nos indica el diagrama
 	private int fila;
+
+	// Métodos get y set
+	public int getFila() {
+		return fila;
+	}
+
+	private void setFila(int fila) {
+		if (fila < 1 || fila > 8) {
+			throw new IllegalArgumentException("ERROR: Fila no válida.");
+
+		} else {
+			this.fila = fila;
+		}
+	}
+
+	public char getColumna() {
+		return columna;
+	}
+
+	private void setColumna(char columna) {
+		if (columna < 'a' || columna > 'h') {
+			throw new IllegalArgumentException("ERROR: Columna no válida.");
+
+		} else {
+			this.columna = columna;
+
+		}
+	}
+
 	private char columna;
 
 }
-
-
