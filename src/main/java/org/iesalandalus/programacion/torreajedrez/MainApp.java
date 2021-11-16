e2322567315f93d050c7b54901d9d801494982f7
Janto7 <72363448+Janto7@users.noreply.github.com>
2021-11-16 13:26:40 +0100

    Commit mostrarTorre()

--- src/main/java/org/iesalandalus/programacion/torreajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/MainApp.java
@@ -1,9 +1,30 @@
 package org.iesalandalus.programacion.torreajedrez;
 
+import javax.naming.OperationNotSupportedException;
+
+/**
+ * @author José Antonio Del Rey Martínez
+ *
+ */
+
+import org.iesalandalus.programacion.utilidades.Entrada;
+
 public class MainApp {
 
+	private static Torre torre;
+	private static boolean salir;
+
 	public static void main(String[] args) {
-		System.out.println("kk");
+
+	}
+
+	// Metodo Mostrar Torre
+	private static void mostrarTorre() {
+		if (torre != null && !salir) {
+			System.out.println("------------------------------------------------------------------------");
+			System.out.println(torre);
+			System.out.println("------------------------------------------------------------------------");
+		}
 	}
 
 }
